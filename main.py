974dfbf5cfce088e62adffc2871c6476b5693bb9
BOOBA24 <barryelhadjboubacar19@gmail.com>
2025-06-02 14:36:52 -0400

    supression

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI,HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+app= FastAPI()
+@app.get("/")
+async def welcome():
+    try:
+        return{"message": "Hello from RCW Teccart"}
+    except Exception as e:
+        print(f'Exception : {e}')
+        raise HTTPException(status_code = 500, detail = str(e))
+    
+@app.get("/")
+async def Bienvenue():
+    try:
+        return{"message": "Hello à mon cours Rcw"}
+    except Exception as e:
+        print(f'Exception : {e}')
+        raise HTTPException(status_code = 500, detail = str(e))
